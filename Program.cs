cebd92593fb09799d42a1dd01aefb620cb214b14
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-06-09 23:56:11 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace String_Based_Enums_InCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
