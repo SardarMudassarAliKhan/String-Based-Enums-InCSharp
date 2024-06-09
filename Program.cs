a1a05318abd1d4ed9ba3c6caecfd88a77b26be8a
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-06-10 00:53:41 +0500

    101- Code for GetEnumValue and GetEnumFromString

--- Program.cs
+++ Program.cs
@@ -1,10 +1,47 @@
-﻿namespace String_Based_Enums_InCSharp
+﻿using System.Runtime.Serialization;
+
+namespace String_Based_Enums_InCSharp
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var  Role = Roles.Admin; 
+           Console.WriteLine("Role Before Conversion to string: "+Role.ToString());
+
+           // Conversion Enum to string 
+           var RoleString = Role.ToString();
+           var EnumToStringValue = GetEnumValue(Role);
+           Console.WriteLine("Role After Conversion to string: "+RoleString);
+
+           // Conversion string to Enum
+           var RoleEnum = GetEnumFromString<Roles>(EnumToStringValue);
+           Console.WriteLine("Role After Conversion to Enum: "+RoleEnum);
+          
+        }
+
+        // Conversion string to Enum
+        public static string GetEnumValue<T>(T enumValue) where T : Enum
+        {
+            var type = enumValue.GetType();
+            var memInfo = type.GetMember(enumValue.ToString());
+            var attributes = memInfo[0].GetCustomAttributes(typeof(EnumMemberAttribute), false);
+            return ((EnumMemberAttribute)attributes[0]).Value;
+        }
+
+        // Conversion Enum to string
+        public static T GetEnumFromString<T>(string value) where T : Enum
+        {
+            var type = typeof(T);
+            foreach (var field in type.GetFields())
+            {
+                var attribute = Attribute.GetCustomAttribute(field, typeof(EnumMemberAttribute)) as EnumMemberAttribute;
+                if (attribute != null && attribute.Value == value)
+                {
+                    return (T)field.GetValue(null);
+                }
+            }
+            throw new ArgumentException($"Unknown value: {value}");
         }
     }
 }
---
+++ Roles.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace String_Based_Enums_InCSharp
+{
+    [DataContract]
+    public enum Roles
+    {
+        [EnumMember(Value = "Admin")]
+        Admin,
+
+        [EnumMember(Value = "Developer")]
+        Developer,
+
+        [EnumMember(Value = "Manager")]
+        Manager,
+
+        [EnumMember(Value = "TeamLead")]
+        TeamLead
+    }
+}
